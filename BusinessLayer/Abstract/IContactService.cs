e0b6adcb6a14b727995c1cb3a08f283360690836
Adem Yıldırım <yldrmadem52@gmail.com>
2023-11-27 13:11:34 +0300

    59.Ders Tamamlandı

---
+++ BusinessLayer/Abstract/IContactService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+  public  interface IContactService
+    {
+        List<Contact> GetAll();
+        void ContactyAdd(Contact contact);
+        void ContactDelete(Contact contact);
+        void ContactyUpdate(Contact contact);
+        Contact GetByID(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void ContactDelete(Contact contact)
+        {
+            _contactDal.Delete(contact);
+        }
+
+        public void ContactyAdd(Contact contact)
+        {
+            _contactDal.Insert(contact);
+        }
+
+        public void ContactyUpdate(Contact contact)
+        {
+            _contactDal.Update(contact);
+        }
+
+        public List<Contact> GetAll()
+        {
+            return _contactDal.List();
+        }
+
+        public Contact GetByID(int id)
+        {
+            return _contactDal.Get(x=>x.ContactID==id);
+        }
+    }
+}
