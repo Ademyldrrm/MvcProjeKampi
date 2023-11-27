5cca0a73782aa149abd76360f0a4671ef155349f
Adem Yıldırım <yldrmadem52@gmail.com>
2023-11-27 21:21:43 +0300

    68.Ders Tamamlandı

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+        IImageDal _ımageDal;
+
+        public ImageFileManager(IImageDal ımageDal)
+        {
+            _ımageDal = ımageDal;
+        }
+
+        public List<ImageFile> GetAll()
+        {
+            return _ımageDal.List();
+        }
+    }
+}
